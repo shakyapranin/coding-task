06f94ce0a3d5eede201fad722c7d5b11176d5c0e
pranin.shakya <pranin@view9.com.np>
2016-07-10 12:15:32 +0545

    <Initiation of CSVOperation facade>

--- resources/views/personnels/index.blade.php
+++ resources/views/personnels/index.blade.php
@@ -7,6 +7,62 @@
                     <div class="panel-heading">
                         Personnels
                     </div>
+                    <div class="panel-body">
+                        <table class="table table-striped task-table">
+
+                            <!-- Table Headings -->
+                            <thead>
+                            <th>Token</th>
+                            <th>Name</th>
+                            <th>Gender</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                            <th>Date of birth</th>
+                            <th>&nbsp;</th>
+                            </thead>
+
+                            <!-- Table Body -->
+                            <tbody>
+                            {{--@foreach ($tasks as $task)--}}
+                                {{--<tr>--}}
+                                    {{--<!-- Task Name -->--}}
+                                    {{--<td class="table-text">--}}
+                                        {{--<div>{{ $task->name }}</div>--}}
+                                    {{--</td>--}}
+
+                                    {{--<td>--}}
+                                        {{--<!-- TODO: Delete Button -->--}}
+                                    {{--</td>--}}
+                                {{--</tr>--}}
+                            {{--@endforeach--}}
+                            <tr>
+                                <!-- Task Name -->
+                                <td class="table-text">
+                                    <div>asdf</div>
+                                </td>
+                                <td class="table-text">
+                                    <div>asdf</div>
+                                </td>
+                                <td class="table-text">
+                                    <div>asdf</div>
+                                </td>
+                                <td class="table-text">
+                                    <div>asdf</div>
+                                </td>
+                                <td class="table-text">
+                                    <div>asdf</div>
+                                </td>
+                                <td class="table-text">
+                                    <div>asdf</div>
+                                </td>
+
+                                <td>
+                                    <!-- TODO: Delete Button -->
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
